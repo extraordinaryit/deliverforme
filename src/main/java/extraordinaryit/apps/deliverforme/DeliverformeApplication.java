ef84dbc2da1960fb20e1a830a6921455c4dd74fa
Thabane <thabanendaba@gmail.com>
2020-05-22 22:14:37 +0200

    Init commit

---
+++ src/main/java/extraordinaryit/apps/deliverforme/DeliverformeApplication.java
@@ -0,0 +1,13 @@
+package extraordinaryit.apps.deliverforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/extraordinaryit/apps/deliverforme/DeliverformeApplicationTests.java
@@ -0,0 +1,13 @@
+package extraordinaryit.apps.deliverforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
